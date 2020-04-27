ec28cc182ac2f8e199c2f33291253d400650c42e
KanatImagambetov <imagambetov.k@gmail.com>
2020-04-27 14:22:48 -0500

    day29_Wrapper_ArrayList

--- src/day29_Wrapper_ArrayList/ValueOfMethod.java
+++ src/day29_Wrapper_ArrayList/ValueOfMethod.java
@@ -1,4 +1,7 @@
 package day29_Wrapper_ArrayList;
+import day19_Arrays.Arrays;
+
+import java.lang.reflect.Array;
 
 public class ValueOfMethod {
     public static void main(String[] args) {
@@ -6,5 +9,30 @@ public class ValueOfMethod {
         Integer a=Integer.valueOf(str);
 
         System.out.println(a+1);
+
+        String r1="truE";
+        boolean a1=Boolean.valueOf(r1);
+        System.out.println(r1);
+        Boolean a2=Boolean.valueOf(r1);
+        System.out.println(a2);
+
+        int maxNum=Integer.MAX_VALUE;
+        System.out.println(maxNum);
+        double maxNum2=Double.MAX_VALUE;
+        System.out.println(maxNum2);
+
+        int minNum=Integer.MIN_VALUE;
+        System.out.println(minNum);
+
+        byte maxNum3=Byte.MAX_VALUE;
+        System.out.println(maxNum3);
+        byte minNum1=Byte.MIN_VALUE;
+        System.out.println(minNum1);
+
+        //=============================================================
+        boolean[] arr=new boolean[3];
+
+
+
     }
 }
