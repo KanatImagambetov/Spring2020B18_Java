5c4a213b6fcefb34ed3e7a412eb115299781f91d
KanatImagambetov <imagambetov.k@gmail.com>
2020-04-28 09:33:51 -0500

    day30_ArrayList

---
+++ src/day30_ArrayList/warmup.java
@@ -0,0 +1,27 @@
+package day30_ArrayList;
+
+import java.util.Arrays;
+
+public class warmup {
+    public static void main(String[] args) {
+        String str="a1b7c3";
+        char[] arr=str.toCharArray();
+
+        System.out.println(Arrays.toString(arr));
+
+        int sum=0;
+        for(char each: arr){
+//1            boolean isDigit=each>=48&& each<=57;
+//1            if(isDigit){
+//2            if(each>=48&& each<=57){
+            if(Character.isDigit(each)){
+                sum+=Integer.parseInt(""+each);
+            }
+        }
+
+        System.out.println(sum);
+
+        boolean a=Character.isAlphabetic('A');
+
+    }
+}
